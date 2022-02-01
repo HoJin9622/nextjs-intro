f5530a99ae7ea264343ddd1f8fd9368c8051ab4b
이호진 <kiss0104040@gmail.com>
2022-02-01 22:15:42 +0900

    #2.1 Fetching Data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,46 @@
-import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import Seo from '../components/Seo'
 
+const API_KEY = 'be935a38bcde877e7e766f591f9ff959'
+
+interface IMovie {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: number[]
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
 export default function Home() {
+  const [movies, setMovies] = useState<IMovie[]>()
+  useEffect(() => {
+    ;(async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json()
+      setMovies(results)
+    })()
+  }, [])
   return (
     <div>
       <Seo title='Home' />
-      <h1 className='active'>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   )
 }
